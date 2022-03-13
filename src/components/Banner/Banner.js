e801f1e7a01474ec443d95f97eefaab20fcf39e5
pratikb0501 <pratikbagmare@gmail.com>
2022-03-13 22:17:32 +0530

    made mobile responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,9 +4,16 @@ import './Banner.css'
 export default function Banner() {
     return (
         <div className="banner-main-div">
-            <div className='banner-poster'>
+            <div className='banner-poster isDesktop'>
                 <img src='https://a0.muscache.com/im/pictures/644d9a47-e28c-4857-ad7b-2dc191cb3507.jpg?im_w=960' alt="poster" />
                 <div className='img-content'>
+                    <h2>Let your curiosity do the booking</h2>
+                    <p><span>I'm flexible</span></p>
+                </div>
+            </div>
+            <div className='banner-mobile isMobile'>
+                <img src='https://a0.muscache.com/im/pictures/644d9a47-e28c-4857-ad7b-2dc191cb3507.jpg?im_w=960' alt="poster" />
+                <div className='img-content-mobile'>
                     <h2>Let your curiosity do the booking</h2>
                     <p><span>I'm flexible</span></p>
                 </div>
